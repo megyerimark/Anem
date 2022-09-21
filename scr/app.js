f8e179ed18afcfdf223fb7d0104da08b64cad34f
Megyeri Mark <megyko1234@gmail.com>
2022-09-21 10:56:04 +0200

    Törlés gomb hozzáadva, de még nem működik

--- scr/app.js
+++ scr/app.js
@@ -1,25 +1,108 @@
-// const { result } = require("lodash");
-const dowloadButton=document.querySelector('#dowloadButton')
-var url = "http://localhost:3000/employees";
+
+const dowloadButton = document.querySelector('#dowloadButton');
+const dolgozoktabla = document.querySelector('#dolgozoktabla');
+const addButton = document.querySelector('#addButton');
+const namee = document.querySelector("#name");
+const salary = document.querySelector("#salary");
+const city =document.querySelector("#city");
+// let delBtn=document.querySelector("#delBtn");
+
+
+
+const host = "http://localhost:3000";
+
+
 
 dowloadButton.addEventListener('click',()=>{
-    console.log('működik');
-})
+    let endpoint ="employees";
+    let url= host  + '/'+endpoint;
+    
 
 
 fetch(url)
-.then(  (response)=>
-    response.json()
-.then(result=> console.log(result[0].name))
-.catch(error=>{
-    console.log('Hiba! A lekérdezés sikertelen');
-    console.log(error);
+.then(response => response.json())
+.then(result=> {console.log(result[0].name);
+    renderTable(result);
 })
 
-);
+
+.catch(error=>{
+    console.log("Hiba! A lekérdezés sikertelen")
+    console.log(error)
+})
+});
 
 
-// .then( result => result.json())
-// .then( data => {
-//     console.log(data);
-// });
+function renderTable(employees) {
+    employees.forEach(employee => {
+        console.log(employee.name)
+
+        let tr = document.createElement("tr");
+        let tdId = document.createElement("td");
+        let tdName = document.createElement("td");
+        let tdSalary = document.createElement("td");
+        let tdcity = document.createElement("td");
+        let tddel = document.createElement("td");
+        let delBtn=document.createElement("button");
+        delBtn.textContent='Törlés';
+        delBtn.addEventListener('click' ,()=>{
+            console.lod("működik");
+        })
+
+       
+    
+        tr.appendChild(tdId);
+        tr.appendChild(tdName);
+        tr.appendChild(tdSalary);
+        tr.appendChild(tdcity);
+        tddel.appendChild(delBtn);
+        tr.appendChild(tddel)
+        dolgozoktabla.appendChild(tr);
+    
+    
+        tdId.innerHTML = employee.id;
+        tdName.innerHTML = employee.name;
+        tdSalary.innerHTML = employee.salary;
+        tdcity.innerHTML = employee.city;
+    })
+    };
+    // console.log(employees[1].name );
+
+   
+
+
+
+addButton.addEventListener('click', ()=>{
+    let endpoint ='employees';
+    let url = host +"/"+ endpoint;
+    let employee = {
+        
+        name:namee.value,
+        salary:salary.value,
+        city:city.value //itt veszi ki az input elemből beírt adatot
+    };
+    fetch(url,{
+        method:'post',  //mindig post petódussal történik a hozzáadás 
+        body:JSON.stringify(employee),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(result =>{
+        console.log(result);
+    })
+})
+
+function deleteEmployee(id){
+    console.log(id);
+    let endpoint ="employees";
+    let url = host + '/' +endpoint +'/' +id;
+    fetch(url)
+    .then(response => response.json)
+    .then(result =>{
+        console.log(result);
+    })
+}
+
+
